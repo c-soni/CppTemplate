dc8ab86662a69a8f742a0859fbe8e57157d9c640
Chintan <chintan.soni4@gmail.com>
2021-04-19 22:58:22 +0530

    [warnings] set up warnings like CppWeekly
    Practically copied the project warning config
    from Jason Turner's repo.
    Took some effort to set up but it finally works.
    P.S. set the CC and CXX environment variables.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <type_traits>
 
-int main()
-{
-    std::cout << "Hello world" << std::endl;
+template <typename Int>
+concept Integral = requires(Int i) {
+    std::is_integral<Int>();
+};
+
+auto add(Integral auto x, Integral auto y) -> Integral auto { return x + y; }
+
+int main() {
+    std::cout << "Hello world " << add(2, 3) << std::endl;
     return 0;
 }
